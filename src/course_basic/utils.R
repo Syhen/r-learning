470461b3167d75ad049306aaa6ed649c1caefb03
syhen <18635823158@163.com>
2021-09-29 23:51:59 +0800

    fix-bug: better_aggregate can work

--- src/course_basic/utils.R
+++ src/course_basic/utils.R
@@ -1,6 +1,6 @@
 data_match_function = function(x, FUN) {
   false_fun = function(e){FALSE}
-  b = tryCatch({FUN(x)}, Warning=false_fun, error=false_fun)
+  b = tryCatch({FUN(x)}, warning=false_fun, error=false_fun)
   return(b)
 }
 
@@ -13,11 +13,16 @@ better_aggregate = function(x, by, FUN) {
   # TODO: iris所有列data_match_function全返回NA
   samples = x[1:10, ]
   for (col in columns) {
-    b = data_match_function(samples, FUN) && 1
-    print(paste(col, b))
+    b = data_match_function(samples[, col], FUN) && 1
     if ((!is.na(b)) && b) {
       choose_columns = c(choose_columns, col)
     }
   }
   return(aggregate(x[, choose_columns], by=by, FUN=FUN))
 }
+
+iris = read.table("input/iris.data", sep=",", header=FALSE)
+print(better_aggregate(iris, by=list(iris$V5), FUN=mean))
+print(better_aggregate(iris$V1, by=list(iris$V5), FUN=mean))
+print(better_aggregate(iris, by=list(iris$V5), FUN=length))
+print(better_aggregate(iris$V5, by=list(iris$V5), FUN=length))
